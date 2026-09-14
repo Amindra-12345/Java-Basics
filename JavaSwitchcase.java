b98fd20a6d5ac15eaf09ab3e1bd0b5d5331761c5
Amindra Diwyanjana <amindradiwyanjana@gmail.com>
2026-09-14 19:02:36 +0530

    The Switvh case Practices

--- JavaSwitchcase.java
+++ JavaSwitchcase.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 public class JavaSwitchcase {
     public static void main(String[] args) {
         
@@ -35,3 +36,42 @@ public class JavaSwitchcase {
 
     }
 }
+=======
+public class JavaSwitchcase {
+    public static void main(String[] args) {
+        
+        int day;
+        System.out.println("Enter a number (1-7) for the day of the week: ");
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        day = scanner.nextInt();
+        scanner.close();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+
+    }
+}
+>>>>>>> 8d6be29 (The Switvh case Practices)
--- LogicalOperators.java
+++ LogicalOperators.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class LogicalOperators {
     public static void main(String[] args) {
         boolean a = true;
@@ -8,34 +7,13 @@ public class LogicalOperators {
         System.out.println("a || b: " + (a || b));
         System.out.println("!a: " + !a);
 
-int x=20;
-int y=30;
+        int x = 20;
+        int y = 30;
 
-if(x>0 && y>0){
-    System.out.println("Both x and y are positive");
-}else{
-    System.out.println("At least one of x or y is not positive");
-}
+        if (x > 0 && y > 0) {
+            System.out.println("Both x and y are positive");
+        } else {
+            System.out.println("At least one of x or y is not positive");
+        }
     }
 }
-=======
-public class LogicalOperators {
-    public static void main(String[] args) {
-        boolean a = true;
-        boolean b = false;
-
-        System.out.println("a && b: " + (a && b));
-        System.out.println("a || b: " + (a || b));
-        System.out.println("!a: " + !a);
-
-int x=20;
-int y=30;
-
-if(x>0 && y>0){
-    System.out.println("Both x and y are positive");
-}else{
-    System.out.println("At least one of x or y is not positive");
-}
-    }
-}
->>>>>>> fb41596 (LOfgical operators)
