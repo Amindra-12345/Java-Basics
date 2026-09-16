4b0515894fa8c6e318a4b20e0e1564aa9a2c0746
Amindra Diwyanjana <amindradiwyanjana@gmail.com>
2026-09-16 13:50:35 +0530

    Constructer Practices

---
+++ OOP/Constructers.java
@@ -0,0 +1,19 @@
+package OOP;
+
+public class Constructers {
+
+    Constructers(){
+        System.out.println("Constructor called");
+    }
+
+    void methode(){
+
+    }
+
+    public static void main(String args[]){
+
+        Constructers c = new Constructers();
+        c.methode();
+
+    }
+}
