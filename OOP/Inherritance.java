9099c06f5b494f3656b07bce6bb95ee928c6d1e8
Amindra Diwyanjana <amindradiwyanjana@gmail.com>
2026-09-16 14:24:35 +0530

    Single inherritace

--- OOP/Inherritance.java
+++ OOP/Inherritance.java
@@ -1,7 +1,31 @@
 package OOP;
+//Single inherrritence
+ class oldPhone{
+    void call(){
+        System.out.println("You can Get an a Call");
+    }
+    void MSG(){
+        System.out.println("You can do a Massages");
+    }
+}
+ class newPhone extends oldPhone{
+    void Explorur(){
+        System.out.println("YOu can explore the internets");
+    }
+}
+
 
 public class Inherritance {
     public static void main(String args[]){
-        
+        oldPhone p1 = new oldPhone();
+        p1.call();
+        p1.MSG();
+
+System.out.println("++++++++++========++++++++++");
+
+        newPhone p2 = new newPhone();
+         p2.call();
+         p2.MSG();
+         p2.Explorur();
     }
 }
